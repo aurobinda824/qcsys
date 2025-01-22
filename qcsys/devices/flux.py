27686cdb76342338af48c0166d6f2a1e8f1d1696
Aurobinda majhi <aurobindo824@gmail.com>
2025-01-22 17:45:49 +0530

    fixed error.

--- qcsys/devices/flux.py
+++ qcsys/devices/flux.py
@@ -1,5 +1,4 @@
 from qcsys.devices.base import FluxDevice
-from qcsys.common.profiling import profile_function, line_profile_function
 
 from flax import struct
 from jax import config
@@ -37,15 +36,15 @@ class Flux(FluxDevice):
     def get_linear_ω(self):
         return jnp.sqrt(8 * self.params['Ec'] * self.params['Ej'])
 
-    @profile_function
+
     def get_H_linear(self):
         phi_ext = self.prams['phi_ext']
         return 0.25 * self.get_linear_ω() * (self.linear_ops['n'] * jnp.transpose(self.linear_ops['n']).conjugate() + self.linear_ops['phi'] * jnp.transpose(self.linear_ops['phi']).conjugate()) + 0.5 * slef.linear_ops['Ec'] * (phi_ext ** 2) - self.linear_ops['Ec'] * self.phi_zpf() * self.linear_ops['phi'] * phi_ext
 
-    @profile_function
+
     def get_H_nonlinear(self):
         return -1 * self.params['Ej'] * jnp.cosm(self.phi_zpf * self.linear_ops['phi']) 
     
-    @profile_function
+
     def get_H_full(self):
         return self.get_H_linear + self.get_H_nonlinear
